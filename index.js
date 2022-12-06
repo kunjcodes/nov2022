6dcd5fde5bbea010fb573068048032992bbb9811
kunjcodes <117122976+kunjcodes@users.noreply.github.com>
2022-12-06 09:39:03 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -21,23 +21,15 @@ function getMessage() {
         4248: { "name": "Komal", "message": "Special, but far, still there for you, why would you beat her up on street with chappal?", "nextcode": "4188", "food": "" },
         4188: { "name": "Honey", "message": "Sweet as _____.", "nextcode": "1032", "food": "When you meet her, have something sweet!" },
         1032: { "name": "Kashish", "message": "HE/SHE cares for you alot! They knows me, kinda! Closest to you still far!", "nextcode": "1637", "food": "Eat one of her fav food!" },
-        1637: { "name": "Activa", "message": "Blue? What's the closest thing to you at the moment? Find the code. ", "nextcode": "6601" },
-        6601: {
-            "message": "Ruk ja! things are loading yet, Your internet is shit!",
-            "name": "Dolly", "nextcode": "5765", "food": ""
+        1637: {
+            "message": "The one who loves chap! Talk to HE/SHE, meet and you should have your next clue!",
+            "name": "Dolly", "nextcode": "5765", "food": "Have Chap"
         },
-        // 6601: {
-        //     "message": "The one who loves chap! Talk to HE/SHE, meet and you should have your next clue!",
-        //     "name": "Dolly", "nextcode": "5765", "food": ""
-        // },
-
-        3406: {
-            "message": "Whom did I meet? Have a coffee with them. Oh wait, Don't you want your next clue?",
-            "nextcode": "2594", "food": "Coffee", "name": "Aishu"
+        5765: {
+            "message": "Whom did I meet? Have a coffee with them.",
+            "name": "Aishu", "nextcode": "2594", "food": "Coffee"
         },
-        2594: { "name": "Ayushi", "message": "Again a mutual. Also friends with someone we both are friends with. Note down the next clue?", "nextcode": "1825", "food": "" },
-        7994: { "name": "Karan", "message": "Activa pe round marne chale? Meet and ask for next clue?", "nextcode": "4248", "food": "" },
-        5766: { "name": "Aditi", "message": "", "nextcode": "", "food": "" }
+        2594: { "name": "Kunj", "message": "Call me!", "nextcode": "", "food": "" },
     }
 
     if (code in textArrayDict) {
